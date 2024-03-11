cedc828c9f2a60ff48dc8de10ae08576ef125324
John Francis <john9francis@gmail.com>
2024-03-10 22:43:30 -0600

    In main, opened an empty window

--- becs/becs.cc
+++ becs/becs.cc
@@ -1,6 +1,7 @@
 ﻿// becs.cc : Defines the entry point for the application.
 //
 
+#include <GLFW/glfw3.h>
 #include "becs.hh"
 
 #include "test.hh"
@@ -10,6 +11,32 @@ using namespace becs;
 
 int main()
 {
+	GLFWwindow* window;
+
+	// init glfw
+	if (!glfwInit()) return -1;
+
+	// create window 
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+	
+	glfwMakeContextCurrent(window);
+	
+	// loop until window is closed
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+	
+	
 	Test::Hello();
 	return 0;
 }
